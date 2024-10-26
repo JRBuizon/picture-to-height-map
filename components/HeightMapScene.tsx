b0a03490e710b3aec87832b4bc7b91873cfa3456
JRBuizon <buizonjeremiah@gmail.com>
2024-10-26 18:38:48 +0800

    feat: translated it into next js :D

--- components/HeightMapScene.tsx
+++ components/HeightMapScene.tsx
@@ -1,24 +1,9 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import * as THREE from 'three'
 
-const generateHeightMap = () => {
-  // create html image
-  return new Promise<Array<number>>((resolve) => {
-    const canvas = document.createElement('canvas')
-    const ctx = canvas.getContext('2d')
-    const img = new Image()
-    img.onload = () => {
-      ctx?.drawImage(img, 0, 0, 1000, 1000)
-      const imageData = ctx?.getImageData(0, 0, 1000, 1000, {
-        colorSpace: 'srgb',
-      })
-      resolve(imageData)
-    }
-    img.src = '/denali.png'
-  })
-}
+const RESOLUTION = 1000
 
 const calculateHeight = (
   r: number,
@@ -33,26 +18,40 @@ const calculateHeight = (
 }
 
 const HeightMapScene = () => {
+  const [imageData, setImageData] = useState<ImageData | undefined>(undefined)
+
+  useEffect(() => {
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+    const img = new Image()
+    img.onload = () => {
+      ctx?.drawImage(img, 0, 0, RESOLUTION, RESOLUTION)
+      const data = ctx?.getImageData(0, 0, RESOLUTION, RESOLUTION, {
+        colorSpace: 'srgb',
+      })
+      setImageData(data)
+    }
+    img.src = '/denali.png'
+  }, [])
+
   const camera = new THREE.PerspectiveCamera()
   camera.far = 5000
   camera.position.set(-110, 102.5, -105)
 
-  const vmapTemp = new Array(1000).fill(0)
-  const vmap = vmapTemp.map(() => new Array(1000))
+  const vmapTemp = new Array(RESOLUTION).fill(0)
+  const vmap = vmapTemp.map(() => new Array(RESOLUTION))
 
-  const plane = useRef()
-
-  React.useEffect(() => {
-    const data = generateHeightMap()
-    data.then((value) => {
-      for (let y = 0; y < 1000; y++) {
-        for (let x = 0; x < 1000; x++) {
-          const index = (x + y * 1000) * 4
-          const index_vertex = (x + y * 1000) * 3
-          const r = value.data[index]
-          const g = value.data[index + 1]
-          const b = value.data[index + 2]
-
+  const plane = useRef<THREE.Mesh<THREE.BufferGeometry<THREE.NormalBufferAttributes>, THREE.Material | THREE.Material[], THREE.Object3DEventMap>>(null)
+  const mesh = useRef<THREE.MeshStandardMaterial>(null)
+  useEffect(() => {
+    if (imageData && plane.current) {
+      for (let y = 0; y < RESOLUTION; y++) {
+        for (let x = 0; x < RESOLUTION; x++) {
+          const index = (x + y * RESOLUTION) * 4
+          const index_vertex = (x + y * RESOLUTION) * 3
+          const r = imageData.data[index]
+          const g = imageData.data[index + 1]
+          const b = imageData.data[index + 2]
           const height = calculateHeight(r, g, b, 500)
           vmap[y][x] = height
           plane.current.geometry.attributes.position.array[index_vertex + 2] =
@@ -60,8 +59,8 @@ const HeightMapScene = () => {
           plane.current.geometry.attributes.position.needsUpdate = true
         }
       }
-    })
-  }, [])
+    }
+  }, [imageData, vmap])
 
   return (
     <Canvas shadows className='bg-black' camera={camera}>
@@ -72,14 +71,15 @@ const HeightMapScene = () => {
         receiveShadow
         castShadow
       >
-        <planeGeometry args={[1000, 1000, 999, 999]} />
+        <planeGeometry args={[RESOLUTION, RESOLUTION, RESOLUTION - 1, RESOLUTION - 1]} />
         <meshStandardMaterial
           wireframe={false}
           color='#3f7b9d'
           side={THREE.DoubleSide}
+          ref={mesh}
         />
       </mesh>
-      <axesHelper args={[1000]} />
+      <axesHelper args={[RESOLUTION]} />
       <OrbitControls />
       <ambientLight intensity={0.3} />
       <hemisphereLight intensity={0.3} castShadow />
