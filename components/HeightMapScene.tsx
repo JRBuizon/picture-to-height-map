86f5a64f5ec5052a4674d39b571a7506a15526d2
JRBuizon <buizonjeremiah@gmail.com>
2024-10-27 11:07:13 +0800

    feat: input image to height map

--- components/HeightMapScene.tsx
+++ components/HeightMapScene.tsx
@@ -1,12 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-import { BufferGeometry, Material, Mesh, MeshStandardMaterial, NormalBufferAttributes, Object3DEventMap } from 'three'
-import { TextureLoader } from "three";
+// import { TextureLoader } from "three";
 import * as THREE from 'three'
-
-const RESOLUTION = 1000
-
+const RESOLUTION = 500
 const calculateHeight = (
   r: number,
   g: number,
@@ -19,22 +16,25 @@ const calculateHeight = (
   return (pixelColor / maxPixelColor) * maxHeight
 }
 
-const HeightMapScene = ({ inputTexture }: { inputTexture: string | null }) => {
+const HeightMapScene = ({ inputImage }: { inputImage: string | null }) => {
   const [imageData, setImageData] = useState<ImageData | undefined>(undefined)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
 
   useEffect(() => {
-    const canvas = document.createElement('canvas')
-    const ctx = canvas.getContext('2d')
-    const img = new Image()
-    img.onload = () => {
-      ctx?.drawImage(img, 0, 0, RESOLUTION, RESOLUTION)
-      const data = ctx?.getImageData(0, 0, RESOLUTION, RESOLUTION, {
-        colorSpace: 'srgb',
-      })
-      setImageData(data)
+    if (canvasRef.current) {
+      const canvas = canvasRef.current
+      const ctx = canvas.getContext('2d')
+      const img = new Image()
+      img.onload = () => {
+        ctx?.drawImage(img, 0, 0, canvas.width, canvas.height)
+        const data = ctx?.getImageData(0, 0, canvas.width, canvas.height, {
+          colorSpace: 'srgb',
+        })
+        setImageData(data)
+      }
+      img.src = inputImage ? inputImage : ''
     }
-    img.src = '/denali.png'
-  }, [])
+  }, [inputImage])
 
   const camera = new THREE.PerspectiveCamera()
   camera.far = 5000
@@ -45,18 +45,6 @@ const HeightMapScene = ({ inputTexture }: { inputTexture: string | null }) => {
 
   const plane = useRef<THREE.Mesh<THREE.BufferGeometry<THREE.NormalBufferAttributes>, THREE.Material | THREE.Material[], THREE.Object3DEventMap>>(null)
   const mesh = useRef<THREE.MeshStandardMaterial>(null)
-  // useEffect(() => {
-  //   if (inputTexture) {
-  //     const textureLoader = new TextureLoader()
-  //     textureLoader.load(inputTexture, (t) => {
-  //       if (mesh.current) {
-  //         mesh.current.displacementMap = t
-  //         mesh.current.map = t
-  //         mesh.current.needsUpdate = true
-  //       }
-  //     })
-  //   }
-  // }, [inputTexture])
   useEffect(() => {
     if (imageData && plane.current) {
       for (let y = 0; y < RESOLUTION; y++) {
@@ -77,38 +65,41 @@ const HeightMapScene = ({ inputTexture }: { inputTexture: string | null }) => {
   }, [imageData, vmap])
 
   return (
-    <Canvas shadows className='bg-black' camera={camera}>
-      <mesh
-        rotation={[Math.PI / 2, 0, 5]}
-        ref={plane}
-        scale={[0.1, 0.1, 0.1]}
-        receiveShadow
-        castShadow
-      >
-        <planeGeometry args={[RESOLUTION, RESOLUTION, RESOLUTION - 1, RESOLUTION - 1]} />
-        <meshStandardMaterial
-          wireframe={false}
-          color='#3f7b9d'
-          side={THREE.DoubleSide}
-          ref={mesh}
-        />
-      </mesh>
-      <axesHelper args={[RESOLUTION]} />
-      <OrbitControls />
-      <ambientLight intensity={0.3} />
-      <hemisphereLight intensity={0.3} castShadow />
-      <directionalLight
-        position={[200, 200, 200]}
-        intensity={5}
-        shadow-mapSize={[1024, 1024]}
-        castShadow
-      >
-        <orthographicCamera
-          attach='shadow-camera'
-          args={[-100, 100, 100, -100]}
-        />
-      </directionalLight>
-    </Canvas>
+    <div className='w-full h-full'>
+      <canvas className='hidden' ref={canvasRef} width={RESOLUTION} height={RESOLUTION} />
+      <Canvas shadows className='bg-black' camera={camera}>
+        <mesh
+          rotation={[Math.PI / 2, 0, 5]}
+          ref={plane}
+          scale={[0.1, 0.1, 0.1]}
+          receiveShadow
+          castShadow
+        >
+          <planeGeometry args={[RESOLUTION, RESOLUTION, RESOLUTION - 1, RESOLUTION - 1]} />
+          <meshStandardMaterial
+            wireframe={false}
+            color='#3f7b9d'
+            side={THREE.DoubleSide}
+            ref={mesh}
+          />
+        </mesh>
+        <axesHelper args={[RESOLUTION]} />
+        <OrbitControls />
+        <ambientLight intensity={0.3} />
+        <hemisphereLight intensity={0.3} castShadow />
+        <directionalLight
+          position={[200, 200, 200]}
+          intensity={5}
+          shadow-mapSize={[1024, 1024]}
+          castShadow
+        >
+          <orthographicCamera
+            attach='shadow-camera'
+            args={[-100, 100, 100, -100]}
+          />
+        </directionalLight>
+      </Canvas>
+    </div>
   )
 }
 
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   transpilePackages: ['three'],
   images: {
+    domains:['localhost'],
     remotePatterns: [
       {
         protocol: 'http',
